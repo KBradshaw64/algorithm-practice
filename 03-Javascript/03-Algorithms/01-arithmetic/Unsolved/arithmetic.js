399317f3e7155e8c4b8f661a8fd3f6c9511bc001
KBradshaw64 <130187631+KBradshaw64@users.noreply.github.com>
2023-10-19 17:52:59 -0700

    added all starting algorithms from class repo

---
+++ 03-Javascript/03-Algorithms/01-arithmetic/Unsolved/arithmetic.js
@@ -0,0 +1,25 @@
+// Fill in the body of each function with the code required
+
+var add = function(num1, num2) {
+    let sum;
+    (num1 + num2) = sum;
+    return sum;
+};
+
+var subtract = function(num1, num2) {
+    let total;
+    (num1 - num2) = total;
+    return total;
+};
+
+var multiply = function(num1, num2) {
+    let product;
+    (num1*num2) = product;
+    return product;
+};
+
+var divide = function(num1, num2) {
+    let quotient;
+    (num1/num2) = quotient;
+    return quotient;
+};
